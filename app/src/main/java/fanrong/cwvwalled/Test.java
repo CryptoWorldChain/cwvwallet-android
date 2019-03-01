7a504676fa2dcbb7378dc5f03a76d66bf459c428
wangxianchao <wangxianchao@icibei.com>
2019-03-01 18:41:55 +0800

    重新编辑过的提交信息

--- app/src/main/java/fanrong/cwvwalled/Test.java
+++ app/src/main/java/fanrong/cwvwalled/Test.java
@@ -3,6 +3,7 @@ package fanrong.cwvwalled;
 import android.app.Activity;
 /**
 测试文件，不会的kotlin 可以在这个写jave 复制到kolin 文件里边，自动转换
+ 这是王现超提交的
 */
 public class Test {
 
@@ -10,5 +11,6 @@ public class Test {
         // 
 "aa".replaceAll(" ","");
     }
+
 
 }
