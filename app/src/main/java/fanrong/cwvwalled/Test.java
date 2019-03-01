a6cbed1246d4126f0d4d84b3da05fba727ee4530
jake Android <small-tree@users.noreply.github.com>
2019-03-01 18:22:52 +0800

    Update Test.java
    test 文件添加描述

--- app/src/main/java/fanrong/cwvwalled/Test.java
+++ app/src/main/java/fanrong/cwvwalled/Test.java
@@ -1,10 +1,13 @@
 package fanrong.cwvwalled;
 
 import android.app.Activity;
-
+/**
+测试文件，不会的kotlin 可以在这个写jave 复制到kolin 文件里边，自动转换
+*/
 public class Test {
 
     public void a(Class<? extends Activity> clazz){
+        // 
 "aa".replaceAll(" ","");
     }
 
